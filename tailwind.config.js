098055895eef371e09c33e808f204e9f7a306d2b
developerleonardo <leo.11hnz@gmail.com>
2024-02-18 16:32:57 -0500

    Initial commit of the Portfolio. I added home page style in the mobile version

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+module.exports = {
+  content: ["./public/index.html", "./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      serif: ["Noto Serif", "serif"]
+    },
+    colors: {
+      ...colors,
+      "primary": "#00F7FF",
+      "secondary": "#23CFD5"
+      
+    },
+    extend: {
+      backgroundImage: {
+        "profilePicture": "url('../img/profilePicture.png')",
+        "profilePictureMobile": "url('../img/profilePictureMobile.jpg')",
+        "reactLogo": "url('../img/react-logo.svg')",
+        "javascript-logo": "url('../img/javascript-logo.svg')",
+        "vite-logo": "url('../img/vite-logo.svg')",
+        "tailwind-logo": "url('../img/tailwind-logo.svg')",
+        "sass-logo": "url('../img/sass-logo.svg')",
+        "css-logo": "url('../img/css-logo.svg')",
+        "html-logo": "url('../img/html-logo.svg')",
+        "git-logo": "url('../img/git-logo.svg')",
+      }
+    },
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+  ],
+}
+
