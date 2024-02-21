d2b3ad5b25d2c277c02b566473aa5a127e776bab
developerleonardo <leo.11hnz@gmail.com>
2024-02-21 16:00:57 -0500

    tailwind.config.js was modified for deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 const colors = require('tailwindcss/colors')
 module.exports = {
-  content: ["./public/index.html", "./src/**/*.{html,js}"],
+  content: ["./public/**/*.html", "./src/**/*.{html,js}"],
   theme: {
     fontFamily: {
       serif: ["Noto Serif", "serif"]
